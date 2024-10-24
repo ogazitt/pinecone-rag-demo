a23d2f8fb7f421c403ea83b094f6d203154c4cad
Omri Gazitt <ogazitt@gmail.com>
2024-10-24 11:56:04 -0700

    updated assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Pinecone - Vercel AI SDK Example',
-  description: 'Pinecone - Vercel AI SDK Example',
+  title: 'Aserto - Pinecone RAG Example',
+  description: 'Aserto - Pinecone RAG Example',
 };
 
 import { Inter } from 'next/font/google';
